e70249e56cb027aa5e9a9787c4402c7ce279434f
MrSuricate <kevindu032270@gmail.com>
2024-12-05 15:49:32 +0100

    [RED] Test attaque

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -6,3 +6,9 @@ class MyTest_Case(unittest.TestCase):
     def test_10_hp_initiaux(self):
         personage = Personage()
         self.assertEqual(20, personage.get_hp())
+
+    def test_attaquer_enleve_1_hp(self):
+        attaquant = Personage()
+        defenseur = Personage()
+        attaquant.attaquer(defenseur)
+        self.assertEqual(19, defenseur.get_hp())
